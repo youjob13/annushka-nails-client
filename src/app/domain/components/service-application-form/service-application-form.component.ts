b1d1a125ee1c1375c1de406d4c0db78f1b7bcfac
Danil Rodionov <Danil_Rodionov@epam.com>
2024-04-21 13:56:43 +0400

    feat/adjust-auth-logic

--- src/app/domain/components/service-application-form/service-application-form.component.ts
+++ src/app/domain/components/service-application-form/service-application-form.component.ts
@@ -1,14 +1,75 @@
-import { NgClass } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { AsyncPipe, NgClass } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  ReactiveFormsModule,
+  FormBuilder,
+  FormsModule,
+} from '@angular/forms';
 import { TuiDay } from '@taiga-ui/cdk';
-import { TuiCalendarModule } from '@taiga-ui/core';
-import { TuiRadioLabeledModule } from '@taiga-ui/kit';
+import {
+  TuiCalendarModule,
+  TuiTextfieldControllerModule,
+} from '@taiga-ui/core';
+import {
+  TuiInputModule,
+  TuiInputPhoneInternationalModule,
+  TuiRadioLabeledModule,
+  TuiSortCountriesPipeModule,
+  TuiTextareaModule,
+} from '@taiga-ui/kit';
+import { TuiCountryIsoCode } from '@taiga-ui/i18n';
 
 const DATE_TIME_MAP = new Map([
   [3, ['11:00', '12:00', '13:00']],
   [5, ['09:00', '11:30', '12:00', '13:00']],
 ]);
+
+type DateFormGroup = FormGroup<{
+  day: FormControl<TuiDay | null>;
+  time: FormControl<string | null>;
+}>;
+
+type ContactsFormGroup = FormGroup<{
+  name: FormControl<string | null>;
+  phone: FormControl<number | null>;
+  comment: FormControl<string | null>;
+}>;
+
+interface FormModel {
+  service: FormControl<string>;
+  date: DateFormGroup;
+  contacts: ContactsFormGroup;
+}
+
+interface IService {
+  id: string;
+  name: string;
+  price: number;
+  description: string;
+}
+
+const SERVICES: IService[] = [
+  {
+    id: '1',
+    name: 'Service 1',
+    price: 25,
+    description: 'Service 1 description',
+  },
+  {
+    id: '2',
+    name: 'Service 2',
+    price: 25,
+    description: 'Service 2 description',
+  },
+  {
+    id: '3',
+    name: 'Service 3',
+    price: 25,
+    description: 'Service 3 description',
+  },
+];
 
 @Component({
   selector: 'ann-service-application-form',
@@ -18,31 +79,105 @@ const DATE_TIME_MAP = new Map([
     TuiCalendarModule,
     TuiRadioLabeledModule,
     ReactiveFormsModule,
+    TuiTextfieldControllerModule,
+    FormsModule,
+    AsyncPipe,
+    TuiInputModule,
+    TuiTextareaModule,
+    TuiInputPhoneInternationalModule,
+    TuiSortCountriesPipeModule,
   ],
   templateUrl: './service-application-form.component.html',
   styleUrl: './service-application-form.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ServiceApplicationFormComponent {
-  protected value: TuiDay | null = null;
+  private readonly fb = inject(FormBuilder);
 
-  items: string[] = [];
+  protected services: IService[] = SERVICES;
+  protected availableTimesForSelectedDay: string[] = [];
 
-  testForm = new FormGroup({
-    testValue1: new FormControl(this.items[0]),
-  });
+  protected selectedDay: TuiDay | null = null;
 
-  public onDayClick(day: TuiDay): void {
-    console.log(day);
-    this.items = DATE_TIME_MAP.get(day.day) ?? [];
-    this.value = day;
+  protected readonly countries = Object.values(TuiCountryIsoCode);
+  protected countryIsoCode = TuiCountryIsoCode.DE;
+
+  protected readonly formModel: FormGroup<FormModel>;
+  protected get serviceControl(): FormModel['service'] {
+    return this.formModel.controls.service;
   }
-
-  public disabledItemHandler = (day: TuiDay) => {
-    return !this.isFreeTimeExists(day.day);
+  protected get dateGroup(): DateFormGroup {
+    return this.formModel.controls.date;
+  }
+  protected get timeControl(): DateFormGroup['controls']['time'] {
+    return this.dateGroup.controls.time;
+  }
+  protected get dayControl(): DateFormGroup['controls']['day'] {
+    return this.dateGroup.controls.day;
+  }
+  protected get contactsGroup(): ContactsFormGroup {
+    return this.formModel.controls.contacts;
+  }
+  protected get userNameControl(): ContactsFormGroup['controls']['name'] {
+    return this.contactsGroup.controls.name;
+  }
+  protected get phoneControl(): ContactsFormGroup['controls']['phone'] {
+    return this.contactsGroup.controls.phone;
+  }
+  protected get commentControl(): ContactsFormGroup['controls']['comment'] {
+    return this.contactsGroup.controls.comment;
+  }
+  public disabledItemHandler = ({ day }: TuiDay) => {
+    return !this.isDayAvailableForApplying(day);
   };
 
-  private isFreeTimeExists(day: TuiDay['day']): boolean {
+  constructor() {
+    this.formModel = this.fb.group<FormModel>({
+      service: this.fb.control('', { nonNullable: true }),
+      date: this.fb.group(
+        {
+          day: this.fb.control(null),
+          time: this.fb.control(null),
+        },
+        { nonNullable: true }
+      ),
+      contacts: this.fb.group(
+        {
+          name: this.fb.control(null),
+          phone: this.fb.control(null),
+          comment: this.fb.control(null),
+        },
+        { nonNullable: true }
+      ),
+    });
+  }
+
+  public submit(): void {
+    console.log(this.formModel.value);
+  }
+
+  public onDayClick(day: TuiDay): void {
+    this.selectedDay = day;
+    this.dayControl.setValue(day);
+
+    this.availableTimesForSelectedDay = this.getTimesForSelectedDay(day.day);
+    if (this.clarifyTimeForDate(this.timeControl)) {
+      this.timeControl.setValue(null);
+    }
+  }
+
+  private clarifyTimeForDate(timeControl: DateFormGroup['controls']['time']) {
+    return (
+      timeControl.value &&
+      !this.availableTimesForSelectedDay.includes(timeControl.value)
+    );
+  }
+
+  private getTimesForSelectedDay(day: TuiDay['day']): string[] {
+    return DATE_TIME_MAP.get(day) ?? [];
+  }
+
+  private isDayAvailableForApplying(day: TuiDay['day']): boolean {
     return DATE_TIME_MAP.has(day);
   }
 }
