97c8e517df53cdb31057df6c0b4a9e2bc603d6d0
Danil Rodionov <Danil_Rodionov@epam.com>
2024-04-27 05:23:30 +0400

    Auto-generated commit

---
+++ ru/chunk-JN6DL7QU.js
@@ -0,0 +1,2 @@
+import{$b as i,Ec as c,Fa as a,ac as o,wc as r}from"./chunk-Z3I3URR3.js";var p=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=a({type:e,selectors:[["ann-admin"]],standalone:!0,features:[c],decls:2,vars:0,template:function(n,m){n&1&&(i(0,"p"),r(1,"admin works!"),o())},changeDetection:0});let t=e;return t})();export{p as AdminComponent};
+/**i18n:f994e39d7e55425eb79f71f36a93b3e1be0e20e3e7b18c9ed2d3946a6fd8aef7*/
